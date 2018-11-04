c15b4fb84b3f4d50df4799fd7a86a0a337849ea9
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-04 17:09:32 +0100

    Change echo to printf for portability & reliability

--- haal.sh
+++ haal.sh
@@ -24,34 +24,30 @@
 # Nov. 2, 2018
 
 haal() {
-  # Helper variables
-  hr="—— 🤖 ——"
-  hr_n="\n—— 🤖 ——\n"
-  editor=""
-
   # Handle Invalid Arguments
-  if [ $# -eq 0 ] # no arguments
+  if [[ $# -eq 0 ]] # no arguments
   then
-    echo "I must take a project URL as argument"
+    printf "🤖: Assign me a project URL please\n"
     return
   elif [[ $1 = "sing" ]] # talented haal
   then
-    echo $hr
-    echo "Daisy, Daisy,\ngive me your answer do."
-    echo "I'm half crazy,\nall for the love of you.\n"
-    echo "It won't be a stylish marriage,\nI can't afford a carriage."
-    echo "But you'll look sweet,\nupon the seat,\nof a bicycle built for two."
+    # printf "%s\n" "$hr"
+    printf "🤖: “Daisy, Daisy,\ngive me your answer do.\n"
+    # printf "“Daisy, Daisy,\ngive me your answer do.\n"
+    printf "I'm half crazy,\nall for the love of you.\n\n"
+    printf "It won't be a stylish marriage,\nI can't afford a carriage.\n"
+    printf "But you'll look sweet,\nupon the seat,\nof a bicycle built for two.”\n"
     return
   elif [[ $1 != https://* ]] # crude URL validation
   then
-    echo "I'm afraid I can't do that. Pass a valid URL"
+    printf "🤖: I'm sorry, $(whoami). I'm afraid I can't do that\n"
     return
   fi
 
   # Download ZIP file from URL
-  echo $hr"\nDownload project ZIP files"
+  # echo $hr
+  printf "🤖: Download project ZIP files\n"
   curl -O ${1}
-
   # Set unzip PATH acc. to optional 2nd argument
   if [ ! -z $2 ]
     then
@@ -66,10 +62,9 @@ haal() {
   # Capture, enter & display unzip PATH
   unzip_path=$(echo $unzip | grep -m1 'creating:' |cut -d ' ' -f5-)
   cd $unzip_path
-  echo $hr_n"Unzip & enter project local folder\n$(pwd)"
-
+  printf "🤖: Unzip & enter project local folder $(pwd)\n"
   # Install project gems
-  echo $hr_n"Install Project gems"
+  printf "🤖: Install Project gems\n"
   bundle install
 
   # Open current folder with local editor
@@ -90,11 +85,12 @@ haal() {
     editor="Vim"
     vi .
   else
-    echo "Sorry, I am afraid I cannot find a code editor"
+    printf "🤖: Sorry $(whoami), I am afraid I cannot find a code editor\n"
   fi
-  echo $hr_n"Open current directory with "$editor
+
+  printf "🤖: Open current directory with $editor\n"
 
   # haal parting tip
-  echo $hr_n"Project set. You can now run your tests by typing:"
-  echo "bundle exec rspec --color\n"
+  printf "🤖: Project set. You can now run your tests by typing:\n"
+  printf "bundle exec rspec --color\n\n"
 }
