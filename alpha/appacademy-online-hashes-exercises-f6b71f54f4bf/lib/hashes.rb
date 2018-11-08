471606c4928744d65a304ece07baa3dc44026b50
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-08 21:38:55 +0100

    Add Hashes section

---
+++ alpha/appacademy-online-hashes-exercises-f6b71f54f4bf/lib/hashes.rb
@@ -0,0 +1,75 @@
+# EASY
+
+# Define a method that, given a sentence, returns a hash of each of the words as
+# keys with their lengths as values. Assume the argument lacks punctuation.
+def word_lengths(str)
+end
+
+# Define a method that, given a hash with integers as values, returns the key
+# with the largest value.
+def greatest_key_by_val(hash)
+end
+
+# Define a method that accepts two hashes as arguments: an older inventory and a
+# newer one. The method should update keys in the older inventory with values
+# from the newer one as well as add new key-value pairs to the older inventory.
+# The method should return the older inventory as a result. march = {rubies: 10,
+# emeralds: 14, diamonds: 2} april = {emeralds: 27, moonstones: 5}
+# update_inventory(march, april) => {rubies: 10, emeralds: 27, diamonds: 2,
+# moonstones: 5}
+def update_inventory(older, newer)
+end
+
+# Define a method that, given a word, returns a hash with the letters in the
+# word as keys and the frequencies of the letters as values.
+def letter_counts(word)
+end
+
+# MEDIUM
+
+# Define a method that, given an array, returns that array without duplicates.
+# Use a hash! Don't use the uniq method.
+def my_uniq(arr)
+end
+
+# Define a method that, given an array of numbers, returns a hash with "even"
+# and "odd" as keys and the frequency of each parity as values.
+def evens_and_odds(numbers)
+end
+
+# Define a method that, given a string, returns the most common vowel. Do
+# not worry about ordering in the case of a tie. Assume all letters are 
+# lower case.
+def most_common_vowel(string)
+end
+
+# HARD
+
+# Define a method that, given a hash with keys as student names and values as
+# their birthday months (numerically, e.g., 1 corresponds to January), returns
+# every combination of students whose birthdays fall in the second half of the
+# year (months 7-12). students_with_birthdays = { "Asher" => 6, "Bertie" => 11,
+# "Dottie" => 8, "Warren" => 9 }
+# fall_and_winter_birthdays(students_with_birthdays) => [ ["Bertie", "Dottie"],
+# ["Bertie", "Warren"], ["Dottie", "Warren"] ]
+def fall_and_winter_birthdays(students)
+end
+
+# Define a method that, given an array of specimens, returns the biodiversity
+# index as defined by the following formula: number_of_species**2 *
+# smallest_population_size / largest_population_size biodiversity_index(["cat",
+# "cat", "cat"]) => 1 biodiversity_index(["cat", "leopard-spotted ferret",
+# "dog"]) => 9
+def biodiversity_index(specimens)
+end
+
+# Define a method that, given the string of a respectable business sign, returns
+# a boolean indicating whether pranksters can make a given vandalized string
+# using the available letters. Ignore capitalization and punctuation.
+# can_tweak_sign("We're having a yellow ferret sale for a good cause over at the
+# pet shop!", "Leopard ferrets forever yo") => true
+def can_tweak_sign?(normal_sign, vandalized_sign)
+end
+
+def character_count(str)
+end
