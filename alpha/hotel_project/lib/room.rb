52ab725d97f7cf9c41ce770cce960f7ce243c2de
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-13 14:38:22 +0100

    Pass Hotel test cases 1/2

--- alpha/hotel_project/lib/room.rb
+++ alpha/hotel_project/lib/room.rb
@@ -1,3 +1,28 @@
+# :nodoc:
 class Room
+  def initialize(capacity)
+    @capacity  = capacity
+    @occupants = []
+  end
 
+  attr_reader :capacity
+  attr_reader :occupants
+
+  def full?
+    return false if occupants.count < capacity
+
+    true
+  end
+
+  def available_space
+    capacity - occupants.count
+  end
+
+  def add_occupant(occupant)
+    unless full?
+      occupants << occupant
+      return true
+    end
+    false
+  end
 end
