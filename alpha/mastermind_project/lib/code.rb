ab9cdc8d948ef7ce9711afa44d7ec3ce30bb2d65
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-14 14:33:36 +0100

    Pass Mastermind test cases

--- alpha/mastermind_project/lib/code.rb
+++ alpha/mastermind_project/lib/code.rb
@@ -1,9 +1,59 @@
+# :nodoc:
 class Code
   POSSIBLE_PEGS = {
-    "R" => :red,
-    "G" => :green,
-    "B" => :blue,
-    "Y" => :yellow
-  }
+    'R' => :red,
+    'G' => :green,
+    'B' => :blue,
+    'Y' => :yellow
+  }.freeze
 
+  def self.valid_pegs?(pegs_arr)
+    pegs_arr.all? { |el| POSSIBLE_PEGS.include?(el.upcase) }
+  end
+
+  def initialize(pegs_arr)
+    raise 'ERROR: Invalid pegs!' unless Code.valid_pegs?(pegs_arr)
+
+    @pegs = pegs_arr.map(&:upcase)
+  end
+
+  attr_reader :pegs
+
+  def self.random(length)
+    random_pegs = []
+    length.times { random_pegs << POSSIBLE_PEGS.keys.sample }
+    Code.new(random_pegs)
+  end
+
+  def self.from_string(pegs_str)
+    Code.new(pegs_str.chars)
+  end
+
+  def [](idx)
+    @pegs[idx]
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def num_exact_matches(code_instance)
+    matches = 0
+    code_instance.pegs.each_index do |idx|
+      matches += 1 if code_instance[idx] == @pegs[idx]
+    end
+    matches
+  end
+
+  def num_near_matches(code_instance)
+    matches = 0
+    code_instance.pegs.each_with_index do |elem, idx|
+      matches += 1 if code_instance[idx] != @pegs[idx] && @pegs.include?(elem)
+    end
+    matches
+  end
+
+  def ==(other)
+    @pegs == other.pegs
+  end
 end
--- alpha/mastermind_project/lib/mastermind.rb
+++ alpha/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
-require_relative "code"
+require_relative 'code'
 
+# :nodoc:
 class Mastermind
+  def initialize(code_length)
+    @secret_code = Code.random(code_length)
+  end
 
+  def print_matches(code_instance)
+    puts "Exact matches: #{@secret_code.num_exact_matches(code_instance)}"
+    puts "Near matches: #{@secret_code.num_near_matches(code_instance)}"
+  end
+
+  def ask_user_for_guess
+    puts 'Enter a code'
+    user_guess = Code.from_string(gets.chomp)
+    print_matches(user_guess)
+    @secret_code == user_guess
+  end
 end
