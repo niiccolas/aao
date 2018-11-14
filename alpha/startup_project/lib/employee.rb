33916dcb3c3ddc56c3fa71c17abdfd86fe93fffb
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-14 09:09:40 +0100

    Pass Startup Employee class test cases

--- alpha/startup_project/lib/employee.rb
+++ alpha/startup_project/lib/employee.rb
@@ -1,3 +1,14 @@
+# :nodoc:
 class Employee
+  attr_reader :name, :title
 
+  def initialize(name, title)
+    @name     = name
+    @title    = title
+    @earnings = 0
+  end
+
+  def pay(salary)
+    @earnings += salary
+  end
 end
