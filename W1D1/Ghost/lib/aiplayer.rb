a48f5ad74e4839ac58ccab3924e9731637c6b43e
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-25 23:48:53 +0100

    Add verbose but working a.i. player class

--- W1D1/Ghost/lib/aiplayer.rb
+++ W1D1/Ghost/lib/aiplayer.rb
@@ -1,28 +1,43 @@
 class AiPlayer
+  attr_reader :name, :type
+
   def initialize
-    @current_fragment = current_fragment
-    @other_players = other_players
+    @name = %w[Casper Jacob Slimey Abe Duppy].sample + ' (AI)'
+    @type = 'ai'
   end
 
-  def words_from_fragment
-    winning_words = @dictionary.select { |word| word.start_with? @fragment }
+  def guess(dic, frag)
+    return ('a'..'z').to_a.sample if frag.empty?
 
-    winning_words.reject! { |word| losing_move.include? word[@fragment.length] }
+    return winning_moves(dic, frag).sample unless winning_moves(dic, frag).empty?
+
+    losing_moves(dic, frag).sample
   end
 
-  def winning_moves
-    return losing_move.sample if winning_words.nil?
+  def winning_moves(dictionary, fragment)
+    winning_words = dictionary.select { |word| word.start_with? fragment }
+    winning_words.reject! { |word| losing_moves(dictionary, fragment).include? word[fragment.length] }
 
     # winning letters, duplicates & nil removed
-    winning_words.keys.map { |word| word[@fragment.length] }.uniq.compact
+    winning_words.keys.map { |word| word[fragment.length] }.uniq.compact
   end
 
-  def losing_moves
-    words_one_letter_away = @dictionary.select do |word|
-      (word.start_with? @fragment) && (word.length == @fragment.length + 1)
+  def losing_moves(dic, frag)
+    words_one_letter_away = dic.select do |word|
+      (word.start_with? frag) && (word.length == frag.length + 1)
     end
 
     # If added to @fragment, those letters would complete a word
     words_one_letter_away.keys.map { |word| word[-1] }
+  end
+
+  def alert_invalid_guess(letter)
+    puts
+    puts "#{letter} is invalid input! Pick a letter of the alphabet,\n"
+    print "while forming an existing word. Try again: "
+    puts
+
+    puts "cancel if this goes in infinite loop"
+    gets.chomp
   end
 end
