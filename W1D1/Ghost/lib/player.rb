db57a7a1daf33b6e4852095eab40bc08e0fef69b
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-25 23:55:22 +0100

    Add type property to differentiate human from a.i. players

--- W1D1/Ghost/lib/player.rb
+++ W1D1/Ghost/lib/player.rb
@@ -1,19 +1,19 @@
 # :nodoc:
 class Player
-  attr_accessor :name
+  attr_accessor :name, :type
 
   def initialize(name)
     @name = name
+    @type = 'human'
   end
 
-  def guess
+  def guess(*args)
     gets.chomp.downcase
   end
 
-  def alert_invalid_guess(letter)
+  def alert_invalid_guess(letter, fragment)
     puts
-    puts "#{letter} is invalid input! Pick a letter of the alphabet,\n"
+    puts "🚫 “#{letter}” is invalid input! Pick a letter of the alphabet,\n"
     print "while forming an existing word. Try again: "
-    puts
   end
 end
