c5c84575c8190afe6317064095760078061243cf
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-11-28 00:03:34 +0100

    Add GhostUi class for separting concerns

---
+++ W1D1/Ghost/lib/ghost_ui.rb
@@ -0,0 +1,89 @@
+class GhostUi
+  def self.clear_screen
+    if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
+      system('cls')
+    else
+      system('clear')
+    end
+  end
+
+  def self.message_to_color(message, color)
+    hues = {
+      red: "\e[31m%s\e[0m",
+      green: "\e[32m%s\e[0m",
+      blue: "\e[34m%s\e[0m"
+    }
+    hues[color.intern] % message
+  end
+
+  def self.display_rules
+    puts '            👻 Welcome to RubyGhost 👻'
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+    puts
+    puts 'In the game of Ghost, each player takes turns'
+    puts 'adding a letter to an ever-growing word fragment.'
+    puts 'Try not to spell a word (of length 3 letters'
+    puts 'or more). Try to force other players to spell'
+    puts 'a word, or try to get a player to say a letter'
+    puts 'that makes it impossible to form a word.'
+    puts
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+  end
+
+  def self.display_standings(players, losses)
+    round_num = losses.values.reduce(:+) + 1
+    puts "            👻 RubyGhost - Round #{round_num} 👻"
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+    puts
+    players.each do |player|
+      ghost_score = '_____'
+      ghost_score[0, losses[player.name]] = ghost_score(player, losses)
+      puts "#{player.name.capitalize}: #{ghost_score}"
+    end
+    puts
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+  end
+
+  def self.ghost_score(player, losses)
+    'GHOST'.slice(0, losses[player.name])
+  end
+
+  def self.display_fragment(fragment)
+    puts GhostUi.message_to_color("Current fragment: #{fragment.upcase}", :green) unless fragment.empty?
+  end
+
+  def self.display_guess_prompt(player, fragment)
+    display_fragment(fragment)
+    print "#{player.name.capitalize}'s turn, pick a letter: "
+  end
+
+  def self.display_ghosted_player(losses)
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+    puts GhostUi.message_to_color("Goodbye #{losses.key(5).capitalize}, you've been ghosted 👻", :red)
+  end
+
+  def self.display_winner(losses)
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+    puts GhostUi.message_to_color("🏅 CONGRATULATIONS #{losses.keys.first.capitalize} YOU WON! ✌️", :blue)
+    puts
+  end
+
+  def self.display_lost_round(fragment, losses, previous_player, players)
+    puts '* * * * * * * * * * * * * * * * * * * * * * * * * *'
+    puts GhostUi.message_to_color("“#{fragment.upcase}” is a word! #{previous_player.name.capitalize} lost this round", :red)
+
+    if players.all? { |player| player.type == 'ai' }
+      # 3 second pause for watching all ai games
+      sleep(3)
+    else
+      # mandatory reflection time before next round
+      # unless a player has just been ghosted
+      unless losses[previous_player.name] == 5
+        loop do
+          print GhostUi.message_to_color('Hit the RETURN key when ready for the next round...', :blue)
+          break if gets == "\n"
+        end
+      end
+    end
+  end
+end
