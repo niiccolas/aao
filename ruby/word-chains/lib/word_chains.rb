bf4f5dea896a3e92230994cd80089b5bba5a3e20
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-12-17 08:47:25 +0100

    Add path building methods

--- ruby/word-chains/lib/word_chains.rb
+++ ruby/word-chains/lib/word_chains.rb
@@ -1,9 +1,8 @@
+# Sanitize user input
 if ARGV.length != 2
-  puts 'Pass two comma-separated words as arguments'
-  ARGV = %w[duck ruby]
-end
-unless ARGV.all? { |el| el.length == ARGV[0].length }
-  puts 'Error! Arguments must be of same length'
+  puts 'Pass two comma-separated words as arguments'; exit
+elsif !ARGV.all? { |el| el.length == ARGV[0].length }
+  puts 'Error! Arguments must be of same length'; exit
 end
 
 # Given two words, builds a chain of words
@@ -33,7 +32,7 @@ class WordChainer
   # For a given word, go thru each index and at that index,
   # test every letter of the alphabet except the current one.
   # If the letter collage creates an existing word,
-  # save it to the adjacent words array.
+  # save it to the adjacent_words array.
   def adjacent_words(word)
     adjacent_words = []
 
@@ -48,4 +47,48 @@ class WordChainer
     end
     adjacent_words
   end
+
+  def run(source, target)
+    @current_words  = [source]
+    @all_seen_words = { source => nil }
+
+    until @current_words.empty?
+      break if @all_seen_words.include?(target)
+
+      explore_current_words
+    end
+
+    build_path(target)
+  end
+
+  def explore_current_words
+    # fill up with new words 1 step away from word in @current_words
+    new_current_words = []
+
+    @current_words.each do |current_word|
+      adjacent_words(current_word).each do |adjacent_word|
+        next if @all_seen_words.include?(adjacent_word)
+
+        @all_seen_words[adjacent_word] = current_word
+        new_current_words << adjacent_word
+      end
+    end
+
+    @current_words = new_current_words
+  end
+
+  def build_path(target)
+    path = []
+    current_word = target
+
+    until current_word.nil?
+      path << current_word
+      current_word = @all_seen_words[current_word]
+    end
+
+    path.reverse
+  end
 end
+
+# Build and display chain of words out of CLI arguments
+p WordChainer.new.run(ARGV[0], ARGV[1])
