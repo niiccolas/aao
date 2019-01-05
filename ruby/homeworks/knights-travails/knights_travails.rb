6473ccf5abddf89e5384e4ed921852d7bfd33856
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-01-05 22:16:08 +0100

    Require the PolyTreeNode class for storing the Knight's initial position

--- ruby/homeworks/knights-travails/knights_travails.rb
+++ ruby/homeworks/knights-travails/knights_travails.rb
@@ -1,7 +1,10 @@
+require_relative '../polytreenode/lib/00_tree_node.rb'
+
 class KnightPathFinder
   attr_accessor :starting_pos
 
   def initialize(position)
     @starting_pos = position
+    @root_node = PolyTreeNode.new(@starting_pos)
   end
 end
