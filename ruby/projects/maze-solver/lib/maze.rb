9649a382858b3b47caf1f90e6bb8aac7f5b25cf1
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-01-15 00:03:26 +0100

    Remove obsolete methods

--- ruby/projects/maze-solver/lib/maze.rb
+++ ruby/projects/maze-solver/lib/maze.rb
@@ -16,7 +16,6 @@ class Maze
     maze = []
     Dir.chdir(File.dirname(__FILE__))
     File.open(maze_filename).each_line do |line|
-    # File.open(maze_filename.first).each_line do |line|
       maze << line.chomp.chars
     end
 
@@ -43,48 +42,6 @@ class Maze
     system('clear')
     maze.each { |row| puts row.join }
     sleep(0.05)
-  end
-
-  def current_column
-    @current_position[1]
-  end
-
-  def mark_current_position
-    @maze[current_row][current_column] = '#'
-  end
-
-  def move_right
-    @current_position[1] = current_column + 1
-  end
-  def move_left
-    @current_position[1] = current_column - 1
-  end
-  def move_up
-    @current_position[0] = current_row - 1
-  end
-  def move_down
-    @current_position[0] = current_row + 1
-  end
-
-  def adjacent_is_exit
-    @maze[current_row][current_column + 1] == 'E' ||
-    @maze[current_row][current_column - 1] == 'E' ||
-    @maze[current_row - 1][current_column] == 'E' ||
-    @maze[current_row + 1][current_column] == 'E'
-  end
-
-  def pathfinder
-    if empty_right
-      move_right
-    elsif empty_up
-      move_up
-    elsif empty_down
-      move_down
-    elsif empty_left
-      move_left
-    end
-
-    mark_current_position
   end
 
   def find_neighbors(point)
@@ -124,24 +81,5 @@ class Maze
       draw_maze
       puts 'There is no way out'.red
     end
-
-  def empty_up
-    @maze[current_row - 1][current_column] == ' '
-  end
-
-  def empty_down
-    @maze[current_row + 1][current_column] == ' '
-  end
-
-  def run_maze_solver
-    system('clear')
-    until adjacent_is_exit
-      pathfinder
-      draw_maze
-      sleep(0.05)
-      system('clear') unless adjacent_is_exit
-    end
-
-    puts '🤖: Exit found!'
   end
 end
