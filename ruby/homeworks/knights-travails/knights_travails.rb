05f0c45e857a429f84f809c1055bc90f88b23448
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-01-16 22:37:39 +0100

    Set knight moves as a class constant

--- ruby/homeworks/knights-travails/knights_travails.rb
+++ ruby/homeworks/knights-travails/knights_travails.rb
@@ -3,6 +3,17 @@ require_relative '../polytreenode/lib/00_tree_node.rb'
 class KnightPathFinder
   attr_accessor :starting_pos, :considered_positions
 
+  KNIGHT_MOVES = [
+    [-1, -2],
+    [-2, -1],
+    [-2,  1],
+    [-1,  2],
+    [ 1, -2],
+    [ 2, -1],
+    [ 2,  1],
+    [ 1,  2]
+  ].freeze
+
   def initialize(position = [0,1])
     @starting_pos = position
     @root_node = PolyTreeNode.new(@starting_pos)
@@ -10,8 +21,6 @@ class KnightPathFinder
   end
 
   def self.valid_moves(pos)
-    deltas = [[-1,-2], [-2, -1], [-2, 1], [-1, 2],
-              [1,-2], [2, -1], [2, 1], [1,2]]
     deltas.each do |delta|
       p delta
     end
