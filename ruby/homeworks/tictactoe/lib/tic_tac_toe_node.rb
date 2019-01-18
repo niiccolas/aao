ae1baab4bb1a21429177e5f39da6005c8b0f7350
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-01-18 19:04:45 +0100

    Set instance variables

--- ruby/homeworks/tictactoe/lib/tic_tac_toe_node.rb
+++ ruby/homeworks/tictactoe/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
