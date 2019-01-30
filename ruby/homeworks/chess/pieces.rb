691d1a62da268043fdaa259889bfdcbbca8b3f67
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-01-30 21:37:59 +0100

    Require piece modules in main pieces file

--- ruby/homeworks/chess/pieces.rb
+++ ruby/homeworks/chess/pieces.rb
@@ -5,9 +5,6 @@ require_relative 'pieces/rook'
 require_relative 'pieces/knight'
 require_relative 'pieces/bishop'
 require_relative 'pieces/pawn'
-require_relative 'pieces/nullpiece'
-
-k = King.new(:white, 'brd', [0,0])
-p k.symbol
-p k.move_diffs
-
+require_relative 'pieces/null_piece'
+require_relative 'pieces/sliding_piece'
+require_relative 'pieces/stepping_piece'
