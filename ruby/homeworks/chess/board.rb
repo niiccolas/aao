1e4ac14499e8da534669824781892006258781b0
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-01-31 21:48:14 +0100

    Initialize the board first with Nullpieces only

--- ruby/homeworks/chess/board.rb
+++ ruby/homeworks/chess/board.rb
@@ -5,7 +5,7 @@ class Board
   attr_accessor :board
 
   def initialize
-    @board = Array.new(8) { Array.new(8) {} }
+    @board = Array.new(8) { Array.new(8, NullPiece.instance) }
     populate_board
   end
 
