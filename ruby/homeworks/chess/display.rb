00821b7cb939f9972fbc79ff58fd1465b4e00cab
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-02-01 23:13:22 +0100

    Add Display methods to render the board and call Cursor class for UI

--- ruby/homeworks/chess/display.rb
+++ ruby/homeworks/chess/display.rb
@@ -3,42 +3,57 @@ require_relative 'cursor'
 require 'colorize'
 
 class Display
-  attr_reader :cursor
+  attr_reader :cursor, :board
   def initialize(board)
     @board  = board
     @cursor = Cursor.new([0, 0], board)
   end
 
   def render
-    puts "  #{('A'..'H').to_a.join(' ')}"
+    puts "  #{('a'..'h').to_a.join(' ')}"
     @board.board.each_with_index do |row, i|
       row_number = (8 - i).to_s + ' '
       print row_number
 
       row.each_with_index do |col, j|
-        if @cursor.cursor_pos == [i, j] && @cursor.selected
-          print col.symbol.green + ' '
-        elsif @cursor.cursor_pos == [i, j]
-          print col.symbol.red + ' '
-        else
-          print col.symbol + ' '
-        end
+        colors = colorize_tile(i, j)
+        print "#{col.symbol.black} ".colorize(colors)
       end
-
-      print ' ' + row_number
       puts
     end
-    puts "  #{('A'..'H').to_a.join(' ')}"
+
+    puts "\nArrows to move cursor,\nSpace bar to select/release"
+  end
+
+  def colorize_tile(i, j)
+    if @cursor.cursor_pos == [i, j] && @cursor.selected
+      bg_color = :red
+    elsif @cursor.cursor_pos == [i, j]
+      bg_color = :green
+    elsif (i + j).odd?
+      bg_color = :light_cyan
+    else
+      bg_color = :light_white
+    end
+    { background: bg_color }
   end
 end
 
 def test_display
-  ch = Display.new(Board.new)
+  chess_board = Display.new(Board.new)
   system('clear')
+  move_to = []
+
   loop do
-    ch.render
-    ch.cursor.get_input
+    chess_board.render
+    current_move = chess_board.cursor.get_input
+    move_to << current_move unless current_move.nil?
     system('clear')
+
+    if move_to.length == 2
+      chess_board.board.move_piece(move_to[0], move_to[1])
+      move_to = []
+    end
   end
 end
 
