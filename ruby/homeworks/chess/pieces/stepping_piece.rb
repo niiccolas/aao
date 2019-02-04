86f9f4f29937cbb1bd39372b5c0df8ca0e58439c
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-02-04 20:19:06 +0100

    Refactor to return an array of valid moves given an object's "pos" ivar

--- ruby/homeworks/chess/pieces/stepping_piece.rb
+++ ruby/homeworks/chess/pieces/stepping_piece.rb
@@ -1,11 +1,25 @@
 module SteppingPiece
   def moves
-    puts
-    puts 'from SteppingPiece'
-    self.move_diffs
+    move_diffs.each_with_object([]) do |(dx, dy), moves|
+      cur_x, cur_y = pos
+      pos = [(cur_x + dx), (cur_y + dy)]
+      next unless board.valid_pos?(pos)
+
+      # Add pos to the moves array (object):
+      # If pos is empty
+      if board.empty?(pos)
+        moves << pos
+      # If pos is occupied by opponent
+      elsif board[pos].color != color
+        moves << pos
+      end
+    end
   end
 
+  private
+
   def move_diffs
+    # method overwritten by subclass!
     raise NotImplementedError
   end
 end
