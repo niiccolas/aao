831984064aa19f2bb25e729e88e7227f587b0d1f
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-02-07 20:23:41 +0100

    Add methods to implement Pawn behavior

--- ruby/homeworks/chess/pieces/pawn.rb
+++ ruby/homeworks/chess/pieces/pawn.rb
@@ -20,4 +20,35 @@ class Pawn < Piece
     row = pos[0]
     [1, 6].include? row
   end
+
+  def forward_dir
+    color == :white ? -1 : 1
+  end
+
+  def forward_steps
+    i, j     = pos
+    one_step = [i + forward_dir, j]
+    return [] unless board.valid_pos?(one_step) && board.empty?(one_step)
+
+    steps     = one_step
+    two_steps = [i + 2 * forward_dir, j]
+
+    steps << two_steps if at_start_row? && board.empty?(two_steps)
+    steps
+  end
+
+  def side_attacks
+    i, j = pos
+    side_moves = [
+      [i + forward_dir, j - 1], [i + forward_dir, j + 1]
+    ]
+
+    side_moves.select do |side_move|
+      next false unless board.valid_pos?(side_move)
+      next false if     board.empty?(side_move)
+
+      opponents_piece = board[side_move]
+      opponents_piece && opponents_piece.color != color
+    end
+  end
 end
