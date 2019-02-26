b8c2688d5719de0cc0a473629a5e0d03cbad2bb3
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-02-26 23:27:51 +0100

    Remove rank from initialize parameters for safety: transfer to a method

--- ruby/projects/poker/lib/card.rb
+++ ruby/projects/poker/lib/card.rb
@@ -1,11 +1,13 @@
 class Card
+  FACES = %i[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+
   attr_reader :suit, :face, :rank
 
-  def initialize(suit, face, rank)
-    @rank     = rank
+  def initialize(suit, face)
     @face     = face
     @suit     = suit
     @revealed = false
+    rank_card
   end
 
   def revealed?
@@ -14,5 +16,11 @@ class Card
 
   def reveal!
     @revealed = true
+  end
+
+  private
+
+  def rank_card
+    @rank = FACES.index(face) + 2
   end
 end
