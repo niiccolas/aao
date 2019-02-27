a709591d06eb78c97b0d19ecff9e97a19710bca0
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-02-27 14:12:36 +0100

    Add the logic for valuing & comparing poker hands

---
+++ ruby/projects/poker/lib/hand.rb
@@ -0,0 +1,139 @@
+require_relative 'deck'
+
+class Hand
+  HANDS = {
+    royal_straight_flush: { name: 'Royal Straight Flush', value: 10 },
+    straight_flush: { name: 'Straight Flush', value: 9 },
+    four_of_kind: { name: 'Four of a kind', value: 8 },
+    full_house: { name: 'Full house', value: 7 },
+    flush: { name: 'Flush', value: 6 },
+    straight: { name: 'Straight', value: 5 },
+    three_of_kind: { name: 'Three of a kind', value: 4 },
+    two_pair: { name: 'Two pairs', value: 3 },
+    pair: { name: 'Pair', value: 2 },
+    high_card: { name: 'High card', value: 1 }
+  }.freeze
+
+  attr_reader :hand
+
+  def initialize(hand = nil)
+    @deck = Deck.new
+    @deck.shuffle!
+    @hand = (hand || @deck.take_cards(5)).sort_by!(&:rank)
+  end
+
+  def draw_hand
+    p hand.map(&:face).zip(hand.map(&:suit))
+  end
+
+  def hand_value
+    hand_methods = HANDS.keys.map(&:to_s).map { |hand| (hand + '?').to_sym }
+
+    # Enumerating thru ::HANDS keys being poker hand names,
+    # send calls to each hand checking method (pair?, two_pair?, etc.).
+    # When current hand is identified, return its :value from ::HANDS
+    hand_methods.each do |hand_method|
+      return HANDS[hand_method.to_s.chop.to_sym][:value] if send(hand_method)
+    end
+  end
+
+  def winning_hand?(*hands)
+    best_hand = hands.all? { |rival| hand_value > rival.hand_value }
+    tie = hands.any? { |rival| hand_value == rival.hand_value }
+    best_kicker = hands.all? { |rival| kicker_value > rival.kicker_value }
+
+    if best_hand
+      true
+    elsif tie
+      best_kicker ? true : false
+    else
+      false
+    end
+  end
+
+  def kicker_value
+    hand.map(&:rank).sum
+  end
+
+  def in_sequence?
+    in_order = ranks.map.with_index do |_el, i|
+      i.zero? ? nil : ranks[i - 1] + 1 == ranks[i]
+    end.drop(1) # remove the first `nil` element for the upcoming Boolean comparison
+
+    in_order.all? { |e| e == true }
+  end
+
+  def same_suit?
+    hand.map(&:suit).uniq.size == 1
+  end
+
+  def straight?
+    in_sequence?
+  end
+
+  def flush?
+    !in_sequence? && same_suit?
+  end
+
+  def straight_flush?
+    in_sequence? && same_suit? &&
+      !royal_straight_flush?
+  end
+
+  def royal_straight_flush?
+    in_sequence? && same_suit? &&
+      hand.map(&:face) == %i[10 J Q K A]
+  end
+
+  def full_house?
+    same_ranks = ranks.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end.values
+
+    same_ranks.sort == [2, 3]
+  end
+
+  def high_card?
+    same_ranks = ranks.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end.values
+
+    same_ranks.count(1) == 5
+  end
+
+  def ranks
+    hand.map(&:rank)
+  end
+
+  def four_of_kind?
+    same_ranks = ranks.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end.values
+
+    same_ranks.count(4) == 1
+  end
+
+  def three_of_kind?
+    same_ranks = ranks.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end.values
+
+    same_ranks.count(3) == 1
+  end
+
+  def two_pair?
+    same_ranks = ranks.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end.values
+
+    same_ranks.count(2) == 2
+  end
+
+  def pair?
+    same_ranks = ranks.each_with_object(Hash.new(0)) do |value, hash|
+      hash[value] += 1
+    end.values
+
+    same_ranks.count(2) == 1
+  end
+end
