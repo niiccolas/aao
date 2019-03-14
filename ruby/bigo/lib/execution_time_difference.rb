6bc0a38fbe1f99ce8c64f7ceff45e69d3afa691d
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-14 15:28:38 +0100

    Add implementations for Execution Time Difference

--- ruby/bigo/lib/execution_time_difference.rb
+++ ruby/bigo/lib/execution_time_difference.rb
@@ -1,18 +1,49 @@
-# Given a list of integers, find the smallest number in the list
-
-# Phase I
-# First, write a function that compares each element to every other element of the list. Return the element if all other elements in the array are larger.
-
-# O(n^2) quadratic time complexity:
+# O(n^2) quadratic time complexity
+# O(1) constant space complexity
 def my_min_quadratic(arr)
   arr.each do |num1|
     smallest = true
     arr.each do |num2|
-      next if num1 == num2
-
       smallest = false if num1 > num2
     end
 
     return num1 if smallest
   end
+end
+
+# O(n) linear time complexity
+# O(1) constant space complexity
+def my_min_linear(arr)
+  min_num = arr.first
+  arr.each { |num| min_num = num if num < min_num }
+
+  min_num
+end
+
+# O(n^2) quadratic time complexity
+def largest_contiguous_subsum_quadratic(arr)
+  subsums = []
+
+  arr.each_index do |i|
+    arr.each_index do |j|
+      subsums << arr[i..j].reduce(:+) unless arr[i..j].empty?
+    end
+  end
+
+  subsums.max
+end
+
+# O(n) linear time complexity
+# O(1) constant space complexity
+# Using Kadane's algorithm https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
+def largest_contiguous_subsum_linear(arr)
+  largest = arr.first
+  current = arr.first
+
+  arr[1..-1].each do |num|
+    current = [(current + num), num].max
+    largest = current if current > largest
+  end
+
+  largest
 end
