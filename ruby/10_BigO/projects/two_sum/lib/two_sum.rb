3e80684b0889f6c07c30efecb94f2e28b4c000eb
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-15 09:23:42 +0100

    Add brute force Two Sum

---
+++ ruby/10_BigO/projects/two_sum/lib/two_sum.rb
@@ -0,0 +1,13 @@
+# O(n^2) quadratic time complexity
+# O(n) linear space complexity
+def bad_two_sum(arr, target_sum)
+  arr.each_with_index do |num1, i|
+    arr[i..-1].each do |num2|
+      next if num1 == num2
+
+      return true if (num1 + num2) == target
+    end
+  end
+
+  false
+end
