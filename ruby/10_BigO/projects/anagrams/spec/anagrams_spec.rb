b6502e0ef0c516737083703c4479da0108e70217
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-15 08:35:37 +0100

    Add Anagrams specs

--- ruby/10_BigO/projects/anagrams/spec/anagrams_spec.rb
+++ ruby/10_BigO/projects/anagrams/spec/anagrams_spec.rb
@@ -1,21 +1,89 @@
 require 'anagrams'
 
-describe '#first_anagram?' do
-  context 'when second string is anagram of the first' do
-    it 'returns true' do
-      expect(first_anagram?('eva', 'ave')).to be true
-      expect(first_anagram?('ales', 'sale')).to be true
-      expect(first_anagram?('elvis', 'lives')).to be true
-      expect(first_anagram?('trance', 'nectar')).to be true
+describe 'Given two strings' do
+  anagrams = [
+    ['eva', 'ave'],
+    ['ales', 'sale'],
+    ['elvis', 'lives'],
+    ['trance', 'nectar']
+  ]
+
+  not_anagrams = [
+    ['eva', 'ava'],
+    ['blue', 'suede'],
+    ['horse', 'harsh'],
+    ['pizzaz', 'fizzle']
+  ]
+
+  describe '#first_anagram?' do
+    context 'when second string is an anagram of the first' do
+      it 'returns true' do
+        anagrams.each do |anagram|
+          expect(first_anagram?(*anagram)).to be true
+        end
+      end
+    end
+
+    context 'else' do
+      it 'returns false' do
+        not_anagrams.each do |not_anagram|
+          expect(first_anagram?(*not_anagram)).to be false
+        end
+      end
     end
   end
 
-  context 'when second string is NOT an anagram of the first' do
-    it 'returns false' do
-      expect(first_anagram?('eva',  'ava')).to be false
-      expect(first_anagram?('blue', 'suede')).to be false
-      expect(first_anagram?('horse', 'harsh')).to be false
-      expect(first_anagram?('pizzaz', 'fizzle')).to be false
+  describe '#second_anagram?' do
+    context 'when second string is an anagram of the first' do
+      it 'returns true' do
+        anagrams.each do |anagram|
+          expect(second_anagram?(*anagram)).to be true
+        end
+      end
+    end
+
+    context 'else' do
+      it 'returns false' do
+        not_anagrams.each do |not_anagram|
+          expect(first_anagram?(*not_anagram)).to be false
+        end
+      end
+    end
+  end
+
+  describe '#third_anagram?' do
+    context 'when second string is an anagram of the first' do
+      it 'returns true' do
+        anagrams.each do |anagram|
+          expect(third_anagram?(*anagram)).to be true
+        end
+      end
+    end
+
+    context 'else' do
+      it 'returns false' do
+        not_anagrams.each do |not_anagram|
+          expect(third_anagram?(*not_anagram)).to be false
+        end
+      end
+    end
+  end
+
+  describe '#fourth_anagram?' do
+    context 'when second string is an anagram of the first' do
+      it 'returns true' do
+        anagrams.each do |anagram|
+          expect(fourth_anagram?(*anagram)).to be true
+        end
+      end
+    end
+
+    context 'else' do
+      it 'returns false' do
+        not_anagrams.each do |not_anagram|
+          expect(fourth_anagram?(*not_anagram)).to be false
+        end
+      end
     end
   end
 end
