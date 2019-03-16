a8fa9287452ecb4ae3591aeafc4bae9bd386af88
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-16 23:18:57 +0100

    Add binary search Two Sum

--- ruby/10_BigO/projects/two_sum/lib/two_sum.rb
+++ ruby/10_BigO/projects/two_sum/lib/two_sum.rb
@@ -1,12 +1,29 @@
+# Brute Force
 # O(n^2) quadratic time complexity
-# O(n) linear space complexity
-def bad_two_sum(arr, target_sum)
+# O(1) constant space complexity, no auxiliary structures are created
+def bad_two_sum?(arr, target_sum)
   arr.each_with_index do |num1, i|
     arr[i..-1].each do |num2|
       next if num1 == num2
 
       return true if (num1 + num2) == target_sum
     end
+  end
+
+  false
+end
+
+# Sorting and Binary Search
+# O(n log n) log-linear time complexity
+# O(n) linear space complexity
+def okay_two_sum?(arr, target_sum)
+  arr.sort!
+  arr.each_with_index do |el, i|
+    match_index = arr.bsearch_index do |value|
+      (target_sum - el) <=> value
+    end
+
+    return true if match_index && match_index != i
   end
 
   false
