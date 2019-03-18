a1779d4f7ca8149deb1267b1c9a108e3a6051033
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-18 19:29:41 +0100

    Add specs for hash map implementation

--- ruby/10_BigO/projects/two_sum/spec/two_sum_spec.rb
+++ ruby/10_BigO/projects/two_sum/spec/two_sum_spec.rb
@@ -59,4 +59,32 @@ context 'Two Sum problem' do
       end
     end
   end
+
+  describe '#two_sum?' do
+    context 'when any two integers in the array amount to the target sum' do
+      it 'returns true' do
+        expect(two_sum?(*has_sum)).to be true
+        expect(two_sum?(*has_sum2)).to be true
+      end
+    end
+
+    context 'else' do
+      it 'returns false' do
+        expect(two_sum?(*has_not_sum)).to be false
+        expect(two_sum?(*has_not_sum2)).to be false
+      end
+    end
+
+    context 'when passed an empty array' do
+      it 'returns false' do
+        expect(two_sum?([], 22)).to eq(false)
+      end
+    end
+
+    context 'when passed an array of length 1' do
+      it 'returns false' do
+        expect(two_sum?([22], 22)).to eq(false)
+      end
+    end
+  end
 end
