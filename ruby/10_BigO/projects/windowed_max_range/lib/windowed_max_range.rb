8841771b1a343845880000899d50e5162304dab6
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-20 00:19:25 +0100

    Add linear implementation

--- ruby/10_BigO/projects/windowed_max_range/lib/windowed_max_range.rb
+++ ruby/10_BigO/projects/windowed_max_range/lib/windowed_max_range.rb
@@ -1,5 +1,7 @@
+require_relative 'min_max_stack_queue'
+
 # Naive approach
-# O(n) linear time complexity
+# O(n * m) linear time complexity
 def windowed_max_range(arr, window_size)
   max_range = nil
   num_windows = (arr.length - window_size) + 1
@@ -9,6 +11,24 @@ def windowed_max_range(arr, window_size)
     current_range = window.max - window.min
 
     max_range = current_range if max_range.nil? || current_range > max_range
+  end
+
+  max_range
+end
+
+# O(n) linear time complexity
+def better_windowed_max_range(arr, window_size)
+  queue = MinMaxStackQueue.new
+  max_range = nil
+
+  arr.each do |el|
+    queue.enqueue(el)
+    queue.dequeue if queue.size > window_size
+
+    if queue.size == window_size
+      current_range = queue.max - queue.min
+      max_range = current_range if max_range.nil? || current_range > max_range
+    end
   end
 
   max_range
