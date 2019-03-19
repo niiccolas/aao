303613f0e5eafcde55c732c194988cabc8e9b80c
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-20 00:13:42 +0100

    Fix & update StackQueue

--- ruby/10_BigO/projects/windowed_max_range/lib/stack_queue.rb
+++ ruby/10_BigO/projects/windowed_max_range/lib/stack_queue.rb
@@ -6,11 +6,13 @@ class StackQueue
     @out_stack = MyStack.new
   end
 
+  # O(1)
   def enqueue(value)
     @in_stack.push(value)
     nil
   end
 
+  # The stack is turned into a queue in O(n). This being the case only once every 2 dequeues, the time complexity is 0(1)
   def dequeue
     if @out_stack.empty?
       @out_stack.push(@in_stack.pop) until @in_stack.empty?
@@ -20,7 +22,7 @@ class StackQueue
   end
 
   def size
-    @in_stack.size && @out_stack.size
+    @in_stack.size + @out_stack.size
   end
 
   def empty?
