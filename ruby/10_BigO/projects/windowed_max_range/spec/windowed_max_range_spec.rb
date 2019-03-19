9789b4b75607497a18c8ba03ec9c54d97b40fbca
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-20 00:16:09 +0100

    Add specs

--- ruby/10_BigO/projects/windowed_max_range/spec/windowed_max_range_spec.rb
+++ ruby/10_BigO/projects/windowed_max_range/spec/windowed_max_range_spec.rb
@@ -14,4 +14,19 @@ context 'Given an array, and a window size' do
       expect(windowed_max_range([1, 0, 2, 5, 4, 8], 3)).to be_an_instance_of(Integer)
     end
   end
+
+  describe '#better_windowed_max_range' do
+    it 'returns the maximum range' do
+      expect(better_windowed_max_range([1, 0, 2, 5, 4, 8], 2)).to eq(4)
+      expect(better_windowed_max_range([1, 2, 3, 5], 3)).to eq(3)
+      expect(better_windowed_max_range([1, 0, 2, 5, 4, 8], 3)).to eq(5)
+      expect(better_windowed_max_range([1, 0, 2, 5, 4, 8], 4)).to eq(6)
+      expect(better_windowed_max_range([1, 3, 2, 5, 4, 8], 5)).to eq(6)
+    end
+
+    it 'returns an Integer' do
+      expect(better_windowed_max_range([1, 0, 2, 5, 4, 8], 2)).to be_an_instance_of(Integer)
+      expect(better_windowed_max_range([1, 0, 2, 5, 4, 8], 3)).to be_an_instance_of(Integer)
+    end
+  end
 end
