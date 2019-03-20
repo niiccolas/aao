87c2cb15d73e426ca22621b261dd2d3745a349f7
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-20 16:51:44 +0100

    Rename UI file

--- ruby/projects/maze-solver/lib/solve_maze.rb
+++
@@ -1,9 +0,0 @@
-require_relative './maze.rb'
-
-if ARGV.empty?
-  puts "Usage:\n  ruby solve_maze.rb maze_filename.txt"
-elsif ARGV[0] !~ /.(txt|TXT)/
-  puts 'File format not supported, use *.txt only'
-else
-  Maze.new(ARGV).run_maze_solver
-end
