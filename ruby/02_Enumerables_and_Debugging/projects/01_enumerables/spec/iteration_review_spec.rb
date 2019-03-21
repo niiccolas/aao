f3cb6020bd965593c4eddf2a8b8bb39b1fb85fad
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-21 22:39:46 +0100

    Remove shoulds from specs

--- ruby/02_Enumerables_and_Debugging/projects/01_enumerables/spec/iteration_review_spec.rb
+++ ruby/02_Enumerables_and_Debugging/projects/01_enumerables/spec/iteration_review_spec.rb
@@ -1,9 +1,8 @@
-# spec/iteration-review.rb
 require 'iteration_review'
 
-describe "#factors" do
+describe '#factors' do
   context "given an integer 'int'" do
-    it 'returns an array of factors for int' do
+    it 'returns an array of its factors' do
       expect(factors(9)).to eq([1, 3])
       expect(factors(45)).to eq([1, 3, 5, 9, 15])
     end
@@ -12,20 +11,20 @@ end
 
 describe Array do
   test_arr = Array.new(12) { rand(1..50) }
-  print test_arr
 
   describe '.bubble_sort!' do
     context 'given an unsorted array' do
-      it 'should return a sorted array' do
+      it 'returns a sorted array' do
         expect(test_arr.bubble_sort!).to eql(test_arr.sort)
+        expect([99, -2, 10, 99.2].bubble_sort!).to eql([-2, 10, 99, 99.2])
       end
 
       test_arr_clone = test_arr.clone
-      it 'should modify the original array' do
+      it 'modifies the original array' do
         expect(test_arr.bubble_sort!).not_to eql(test_arr_clone)
       end
 
-      it 'should take an optional block' do
+      it 'can take an optional block' do
         expect(test_arr.bubble_sort! { |num1, num2| num1 <=> num2 } ).to eql(test_arr.sort)
       end
     end
@@ -33,15 +32,15 @@ describe Array do
 
   describe '.bubble_sort' do
     context 'given an unsorted array' do
-      it 'should return a sorted array' do
+      it 'returns a sorted array' do
         expect(test_arr.bubble_sort).to eql(test_arr.sort)
       end
 
-      it 'should NOT modify the original array' do
+      it 'does NOT modify the original array' do
         expect(test_arr.bubble_sort).to eql(test_arr)
       end
 
-      it 'should take an optional block' do
+      it 'can take an optional block' do
         expect(test_arr.bubble_sort { |num1, num2| num2 <=> num1 }).to eql(test_arr.sort.reverse)
       end
     end
@@ -50,7 +49,7 @@ end
 
 describe "#substrings" do
   context "given a string" do
-    it 'should return al the substrings of that string' do
+    it 'returns all the substrings of that string' do
       expect(substrings("fat")).to eq(["f", "fa", "fat", "a", "at", "t"])
       expect(substrings("foam")).to eq(["f", "fo", "foa", "foam", "o", "oa", "oam", "a", "am", "m"])
     end
@@ -59,11 +58,11 @@ end
 
 subword_call = subwords("seassoucat..sdqfoamazeazecat", ["cat","sea","foam"])
 describe "#subwords" do
-  context "given a string and a dictionary (array) of existing words" do
-    it "should return substrings that are actual words" do
+  context "given a string and an array of existing words" do
+    it "returns substrings that are actual words" do
       expect(subword_call).to eq(["sea", "cat", "foam"])
     end
-    it "should NOT include words that are not in the dictionary" do
+    it "rejects words that are not in the dictionary" do
       expect(subword_call).not_to include("ecat", "seassou")
     end
     it 'does NOT match tangled words' do
