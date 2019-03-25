b20ee072db3a2dd594b62d6ac08fa207ce37b1bb
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-03-25 23:24:43 +0100

    Add Game class dealing with the game logic

---
+++ ruby/03_reference/projects/03_sudoku/lib/game.rb
@@ -0,0 +1,33 @@
+require_relative 'board'
+require 'colorize'
+
+class Game
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def play
+    loop do
+      @board.render
+      break if @board.solved?
+
+      begin
+        user_input = prompt
+        @board[*user_input[:pos]] = user_input[:val][0]
+      rescue RuntimeError => e
+        puts "Error: #{e.message}. Press enter to continue".red
+        gets
+      end
+    end
+  end
+
+  def prompt
+    %i[pos val].each.with_object({}) do |key, user_input|
+      print key == :pos ? 'Position? (e.g. 91): ' : 'Value? (1-9):    '
+
+      user_input[key] = gets.chomp.chars.map(&:to_i)
+    end
+  end
+end
