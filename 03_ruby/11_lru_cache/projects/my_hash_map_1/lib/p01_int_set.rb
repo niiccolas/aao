749f322ead632d084102ef021079d4739d428689
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-04 21:34:30 +0200

    Add IntSet implementation

--- 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p01_int_set.rb
+++ 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p01_int_set.rb
@@ -1,44 +1,56 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    (0..@store.length).include? num
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
-
 class IntSet
   def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) { [] }
   end
 
   def insert(num)
+    self[num] << num unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include? num
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -50,23 +62,37 @@ class ResizingIntSet
   attr_reader :count
 
   def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) { [] }
     @count = 0
   end
 
   def insert(num)
+    resize! if num + 1 > num_buckets
+
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete num
+      @count -= 1
+    end
   end
 
   def include?(num)
+    return false if num.nil?
+
+    self[num].include? num
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +100,6 @@ class ResizingIntSet
   end
 
   def resize!
+    @store.concat(Array.new(num_buckets) { [] })
   end
 end
