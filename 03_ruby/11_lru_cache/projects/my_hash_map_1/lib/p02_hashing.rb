8c3584e25bdbc4830a7c95494001851a96b75f3d
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-04 21:35:14 +0200

    Add Hashing functions implementations

--- 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p02_hashing.rb
+++ 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    each_with_index.inject(0) do |temp_hash, (el, i)|
+      (el.hash + i.hash) ^ temp_hash
+    end
+    # map(&:ord).join.to_i.hash
   end
 end
 
 class String
   def hash
+    # chars.hash.hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +22,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    flatten.map(&:to_s).map(&:ord).sort.join.to_i
   end
 end
+
+
+
+
+# [2,3,4].each_with_index.inject(0) do |interm, (e, i)|
+#   e ^ interm
+# end
