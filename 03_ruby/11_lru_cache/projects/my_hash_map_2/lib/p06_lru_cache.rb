de3846c9ece71e716a37122e5f48403114e56c31
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-06 16:13:19 +0200

    Add LRU Cache implementation

--- 03_ruby/11_lru_cache/projects/my_hash_map_2/lib/p06_lru_cache.rb
+++ 03_ruby/11_lru_cache/projects/my_hash_map_2/lib/p06_lru_cache.rb
@@ -1,12 +1,12 @@
-require_relative 'p05_hash_map'
-require_relative 'p04_linked_list'
+require_relative '../../my_hash_map_1/lib/p04_linked_list.rb'
+require_relative '../../my_hash_map_1/lib/p05_hash_map.rb'
 
 class LRUCache
   def initialize(max, prc)
-    @map = HashMap.new
+    @map   = HashMap.new
     @store = LinkedList.new
-    @max = max
-    @prc = prc
+    @max   = max
+    @prc   = prc
   end
 
   def count
@@ -14,22 +14,39 @@ class LRUCache
   end
 
   def get(key)
+    if @map[key]
+      node = @map[key]
+      update_node!(node)
+      node.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
-    'Map: ' + @map.to_s + '\n' + 'Store: ' + @store.to_s
+    'Map: ' + @map.to_s + "\n" + 'Store: ' + @store.to_s + "\n"
   end
 
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    new_val   = @prc.call(key)
+    new_node  = @store.append(key, new_val)
+    @map[key] = new_node
+
+    eject! if count > @max
+    new_val
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    @map[node.key] = @store.append(node.key, node.val)
   end
 
   def eject!
+    node_to_go = @store.first
+    node_to_go.remove
+    @map.delete(node_to_go.key)
+    nil
   end
 end
