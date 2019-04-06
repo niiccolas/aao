be6b4107da9fc641dfc894e82fdede89fc3c6d24
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-06 11:47:13 +0200

    Add HashMap implementation

--- 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p05_hash_map.rb
+++ 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,27 +11,43 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if @count >= num_buckets
+
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      @count += 1
+      bucket(key).append(key, val)
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    @count -= 1
+    bucket(key).remove(key)
   end
 
   def each
+    return nil unless block_given?
+
+    @store.each do |bucket|
+      bucket.each { |node| yield [node.key, node.val] }
+    end
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k} => #{v}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}" # pretty prints HashMap
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +59,16 @@ class HashMap
   end
 
   def resize!
+    old_store = @store
+    @store    = Array.new(num_buckets * 2) { LinkedList.new }
+    @count    = 0
+
+    old_store.each do |bucket|
+      bucket.each { |node| set(node.key, node.val) }
+    end
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
