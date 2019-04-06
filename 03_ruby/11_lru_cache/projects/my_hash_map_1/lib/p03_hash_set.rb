9d34e4aad5f3cd6323f1e3652226fb1a5343d564
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-06 11:45:47 +0200

    Add HashSet implementation

--- 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p03_hash_set.rb
+++ 03_ruby/11_lru_cache/projects/my_hash_map_1/lib/p03_hash_set.rb
@@ -2,23 +2,35 @@ class HashSet
   attr_reader :count
 
   def initialize(num_buckets = 8)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) { [] }
     @count = 0
   end
 
   def insert(key)
+    key_size = key.is_a?(Integer) ? key : key.size
+    resize! if key_size + 1 > num_buckets
+
+    unless include?(key)
+      self[key.hash % num_buckets] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key.hash % num_buckets].include? key
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash % num_buckets].delete key
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +38,6 @@ class HashSet
   end
 
   def resize!
+    @store.concat(Array.new(num_buckets) { [] })
   end
 end
