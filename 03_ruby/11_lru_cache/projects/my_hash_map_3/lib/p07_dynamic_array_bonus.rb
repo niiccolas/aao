cd867af0823ea79ac5fe4616d2099b90e9d4dc93
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-07 20:22:18 +0200

    Add Dynamic Array implementation

--- 03_ruby/11_lru_cache/projects/my_hash_map_3/lib/p07_dynamic_array_bonus.rb
+++ 03_ruby/11_lru_cache/projects/my_hash_map_3/lib/p07_dynamic_array_bonus.rb
@@ -7,37 +7,61 @@ class StaticArray
 
   def [](i)
     validate!(i)
-    self.store[i]
+    store[i]
   end
 
   def []=(i, val)
     validate!(i)
-    self.store[i] = val
+    store[i] = val
   end
 
   def length
-    self.store.length
+    store.length
   end
 
   private
 
   def validate!(i)
-    raise "Overflow error" unless i.between?(0, self.store.length - 1)
+    raise 'Overflow error' unless i.between?(0, store.length - 1)
   end
 end
 
 class DynamicArray
-  attr_accessor :count
+  include Enumerable
+
+  attr_accessor :count, :insertions
 
   def initialize(capacity = 8)
-    @store = StaticArray.new(capacity)
-    @count = 0
+    @store     = StaticArray.new(capacity)
+    @count     = 0
+    @start_idx = 0
   end
 
   def [](i)
+    return nil if i >= @count
+
+    if i.negative?
+      return nil if i < -@count
+      return self[@count + i]
+    end
+
+    @store[(@start_idx + i) % capacity]
   end
 
   def []=(i, val)
+    if i >= @count
+      (i - @count).times { push(nil) } # when over-indexing
+    elsif i.negative?
+      return nil if i < -@count
+      return self[@count + i] = val
+    end
+
+    if i == @count
+      resize! if capacity == @count
+      @count += 1
+    end
+
+    @store[(@start_idx + i) % capacity] = val
   end
 
   def capacity
@@ -45,36 +69,70 @@ class DynamicArray
   end
 
   def include?(val)
+    any? { |el| el == val }
   end
 
   def push(val)
+    resize! if capacity == @count
+    @store[(@start_idx + @count) % capacity] = val
+    @count += 1
+    self
   end
 
   def unshift(val)
+    resize! if capacity == @count
+    @start_idx = (@start_idx - 1) % capacity
+    @store[@start_idx] = val
+    @count += 1
+    self
   end
 
   def pop
+    return nil if count.zero?
+
+    last_element = @store[(@start_idx + @count - 1) % capacity]
+    @count -= 1
+    last_element
   end
 
   def shift
+    return nil if @count.zero?
+
+    @count -= 1
+    first_element = @store[@start_idx]
+    @start_idx    = (@start_idx + 1) % capacity
+    first_element
   end
 
   def first
+    return nil if @count.zero?
+
+    @store[@start_idx]
   end
 
   def last
+    return nil if @count.zero?
+
+    @store[(@start_idx + @count - 1) % capacity]
   end
 
   def each
+    return nil unless block_given?
+
+    @count.times { |i| yield self[i] }
+    self
   end
 
   def to_s
-    "[" + inject([]) { |acc, el| acc << el }.join(", ") + "]"
+    '[' + inject([]) { |acc, el| acc << el }.join(', ') + ']'
   end
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    return false unless length == other.length
+
+    capacity.times { |i| return false unless @store[i] == other[i] }
+    true
   end
 
   alias_method :<<, :push
@@ -83,5 +141,10 @@ class DynamicArray
   private
 
   def resize!
+    new_store = StaticArray.new(capacity * 2)
+    each_with_index { |el, i| new_store[i] = el }
+
+    @store     = new_store
+    @start_idx = 0
   end
 end
