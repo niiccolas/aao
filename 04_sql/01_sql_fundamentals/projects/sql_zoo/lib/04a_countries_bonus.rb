3829da1311c7b044f3065529f9d50f6fe630f542
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-10 17:25:30 +0200

    Add aggregate queries

--- 04_sql/01_sql_fundamentals/projects/sql_zoo/lib/04a_countries_bonus.rb
+++ 04_sql/01_sql_fundamentals/projects/sql_zoo/lib/04a_countries_bonus.rb
@@ -17,6 +17,16 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      countries.name
+    FROM
+      countries
+    WHERE
+      countries.gdp > (
+        SELECT MAX(gdp)
+        FROM   countries
+        WHERE  continent = 'Europe'
+      );
   SQL
 end
 
@@ -24,6 +34,18 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT DISTINCT
+      countries.continent,
+      countries.name,
+      countries.area
+    FROM
+      countries
+    WHERE
+      area IN (
+        SELECT   MAX(area)
+        FROM     countries
+        GROUP BY continent
+      );
   SQL
 end
 
@@ -31,5 +53,17 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      populous.name,
+      populous.continent
+    FROM
+      countries AS populous
+    WHERE
+      populous.population > 3 * (
+        SELECT MAX(sparse.population)
+        FROM   countries AS sparse
+        WHERE  sparse.name != populous.name AND
+               sparse.continent = populous.continent
+      );
   SQL
 end
