0edf276c46e5b6e00c4aae226a58fd0f591392d4
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-13 18:12:13 +0200

    Add user & question follows to seed

--- 04_sql/02_object_relational_model/projects/02_aa_questions/import_db.sql
+++ 04_sql/02_object_relational_model/projects/02_aa_questions/import_db.sql
@@ -21,7 +21,8 @@ INSERT INTO
 VALUES
   ('Wendy', 'Glover'),
   ('Gina', 'Ratke'),
-  ('Owen', 'Greenholt');
+  ('Owen', 'Greenholt'),
+  ('Arlie', 'Russel');
 
 -- questions (BELONGS TO users)
 CREATE TABLE questions (
@@ -68,7 +69,16 @@ VALUES
   ((SELECT id FROM users WHERE fname = 'Wendy' AND lname = 'Glover'),
    (SELECT id FROM questions WHERE title = 'Owen: Roommates')),
 
+  ((SELECT id FROM users WHERE fname = 'Wendy' AND lname = 'Glover'),
+   (SELECT id FROM questions WHERE title = 'Gina: NodeJS FTW')),
+
+  ((SELECT id FROM users WHERE fname = 'Wendy' AND lname = 'Glover'),
+   (SELECT id FROM questions WHERE title = 'Gina: Ruby stil relevant?')),
+
   ((SELECT id FROM users WHERE fname = 'Gina' AND lname = 'Ratke'),
+   (SELECT id FROM questions WHERE title = 'Owen: Roommates')),
+
+  ((SELECT id FROM users WHERE fname = 'Arlie' AND lname = 'Russel'),
    (SELECT id FROM questions WHERE title = 'Owen: Roommates')),
 
   ((SELECT id FROM users WHERE fname = 'Owen' AND lname = 'Greenholt'),
