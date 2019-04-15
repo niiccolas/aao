f2bd24d0d46d7ef1c5b2136e0a1b9cc47fb7a7f1
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-16 00:00:31 +0200

    Add question n likes query

--- 04_sql/02_object_relational_model/projects/02_aa_questions/questions.rb
+++ 04_sql/02_object_relational_model/projects/02_aa_questions/questions.rb
@@ -60,6 +60,14 @@ class Question
   def followers
     QuestionFollow.followers_for_question_id(@id)
   end
+
+  def likers
+    QuestionLike.likers_for_question_id(@id)
+  end
+
+  def num_likes
+    QuestionLike.num_likes_for_question_id(@id)
+  end
 end
 
 class User
