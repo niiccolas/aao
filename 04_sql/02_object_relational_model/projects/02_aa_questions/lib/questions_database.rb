8c789b2db91f5ff8b9619a729b09dc3e85064e47
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-18 13:53:43 +0200

    Set skeleton for RSPEC test files

--- 04_sql/02_object_relational_model/projects/02_aa_questions/lib/questions_database.rb
+++ 04_sql/02_object_relational_model/projects/02_aa_questions/lib/questions_database.rb
@@ -5,6 +5,7 @@ class QuestionsDatabase < SQLite3::Database
   include Singleton # enforces a unique connection to the DB
 
   def initialize
+    Dir.chdir(File.dirname(__FILE__))
     super('questions.db')
     self.type_translation = true
     self.results_as_hash  = true
