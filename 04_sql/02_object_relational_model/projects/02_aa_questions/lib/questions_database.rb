22109bc94d6948e59deb2ec7e7d475a3a5cad6b1
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-21 07:20:43 +0200

    Add methods for DRY-er DB queries

--- 04_sql/02_object_relational_model/projects/02_aa_questions/lib/questions_database.rb
+++ 04_sql/02_object_relational_model/projects/02_aa_questions/lib/questions_database.rb
@@ -10,4 +10,16 @@ class QuestionsDatabase < SQLite3::Database
     self.type_translation = true
     self.results_as_hash  = true
   end
+
+  def self.get_first_row(*args)
+    instance.get_first_row(*args)
+  end
+
+  def self.get_first_value(*args)
+    instance.get_first_value(*args)
+  end
+
+  def self.execute(*args)
+    instance.execute(*args)
+  end
 end
