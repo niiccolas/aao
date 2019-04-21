59e6cb579422e850707f6f6aaf6ffb31d0ebbc2a
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-04-21 07:17:18 +0200

    Refactor queries for legibility

--- 04_sql/02_object_relational_model/projects/02_aa_questions/lib/question_follow.rb
+++ 04_sql/02_object_relational_model/projects/02_aa_questions/lib/question_follow.rb
@@ -1,49 +1,60 @@
 require_relative 'questions_database'
-require_relative 'user'
 require_relative 'question'
+require_relative 'user'
 
-class QuestionFollow
-  def self.find_by_id(id)
-    qst_follow = QuestionsDatabase.instance.execute(<<-SQL, id)
-    SELECT * FROM question_follows WHERE question_follows.id = ?
-    SQL
-    return nil if qst_follow.empty?
-
-    QuestionFollow.new(qst_follow.first)
-  end
-
+class QuestionFollow < ModelBase
   def self.followers_for_question_id(question_id)
-    followers = QuestionsDatabase.instance.execute(<<-SQL, question_id)
-    SELECT * FROM users
-    JOIN question_follows ON question_follows.user_id = users.id
-    WHERE question_follows.question_id = ?;
+    followers = QuestionsDatabase.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        users
+      JOIN
+        question_follows
+      ON
+        question_follows.user_id = users.id
+      WHERE
+        question_follows.question_id = ?;
     SQL
-    return nil if followers.empty?
 
-    followers.map { |follower| User.new(follower) }
+    followers.empty? ? nil : followers.map { |follower| User.new(follower) }
   end
 
   def self.followed_questions_for_user_id(user_id)
-    questions = QuestionsDatabase.instance.execute(<<-SQL, user_id)
-    SELECT * FROM questions
-    JOIN question_follows ON question_follows.question_id = questions.id
-    WHERE question_follows.user_id = ?;
+    questions = QuestionsDatabase.execute(<<-SQL, user_id)
+      SELECT
+        *
+      FROM
+        questions
+      JOIN
+        question_follows
+      ON
+        question_follows.question_id = questions.id
+      WHERE
+        question_follows.user_id = ?;
     SQL
-    return nil if questions.empty?
 
-    questions.map { |question| Question.new(question) }
+    questions.empty? ? nil : questions.map { |question| Question.new(question) }
   end
 
   # Default to top 5
   def self.most_followed_questions(n_questions = 5)
-    mf_questions = QuestionsDatabase.instance.execute(<<-SQL, n_questions)
-    SELECT * FROM questions
-    JOIN question_follows ON question_follows.question_id = questions.id
-    GROUP BY question_id
-    ORDER BY COUNT(user_id) DESC
-    LIMIT ?;
+    mf_questions = QuestionsDatabase.execute(<<-SQL, n_questions)
+    SELECT
+      questions.*
+    FROM
+      questions
+    JOIN
+      question_follows
+    ON
+      question_follows.question_id = questions.id
+    GROUP BY
+      question_follows.question_id
+    ORDER BY
+      COUNT(question_follows.user_id) DESC
+    LIMIT
+      ?;
     SQL
-    return nil if mf_questions.empty?
 
     mf_questions.map { |question| Question.new(question) }
   end
