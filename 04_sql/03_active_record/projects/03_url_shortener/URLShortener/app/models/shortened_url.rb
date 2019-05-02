fd9c8871d45076a27a168433bc8a94679779afe6
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-05-02 19:10:32 +0200

    Add association to User class & url gen method

--- 04_sql/03_active_record/projects/03_url_shortener/URLShortener/app/models/shortened_url.rb
+++ 04_sql/03_active_record/projects/03_url_shortener/URLShortener/app/models/shortened_url.rb
@@ -1,5 +1,34 @@
+# shortened_urls
+# table schema:
+
+# id           :integer     not null, primary key
+# long_url     :string      not null
+# short_url    :string      not null
+# submitter_id :integer     not null
+# created_at   :datetime    not null
+# updated_at   :datetime    not null
+
 class ShortenedUrl < ApplicationRecord
-  validates :long_url, :short_url, :submitter_id, presence: true
+  validates :long_url, :short_url, :submitter, presence: true
   validates :short_url, uniqueness: true
 
+  belongs_to :submitter,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :submitter_id
+
+  def self.random_code
+    loop do
+      random_url = SecureRandom.urlsafe_base64(16)
+      return random_url unless ShortenedUrl.exists?(short_url: random_url)
+    end
+  end
+
+  def self.create_short_url(user, long_url)
+    ShortenedUrl.create!(
+      submitter_id: user.id,
+      long_url: long_url,
+      short_url: ShortenedUrl.random_code
+    )
+  end
 end
