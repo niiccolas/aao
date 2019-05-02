ad324bd6b2b4950020ecbd96a9d922e55cdfd649
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-05-02 13:00:09 +0200

    Add models AR associations

--- 04_sql/03_active_record/projects/02_associations/app/models/enrollment.rb
+++ 04_sql/03_active_record/projects/02_associations/app/models/enrollment.rb
@@ -1,2 +1,13 @@
 class Enrollment < ApplicationRecord
+  # association for User and Course
+  # has many Users
+  belongs_to :user,
+    class_name: 'User',
+    foreign_key: :student_id,
+    primary_key: :id
+
+  belongs_to :course,
+    class_name: 'Course',
+    foreign_key: :course_id,
+    primary_key: :id
 end
