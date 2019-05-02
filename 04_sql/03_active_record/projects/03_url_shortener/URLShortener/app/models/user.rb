2d0b7d8de0f071a4b5825d915d47372ea3e6cbb3
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-05-02 19:09:33 +0200

    Add association to ShortenedUrl

--- 04_sql/03_active_record/projects/03_url_shortener/URLShortener/app/models/user.rb
+++ 04_sql/03_active_record/projects/03_url_shortener/URLShortener/app/models/user.rb
@@ -1,5 +1,15 @@
+# users
+# table schema:
+
+# id        :integer    not null, primarykey
+# email     :string     not null, unique
+
 class User < ApplicationRecord
   # Add uniqueness and presence validations. Without these, people might go about creating user accounts without emails. We cannot tolerate such misbehavior.
-
   validates :email, presence: true, uniqueness: true
+
+  has_many :submitted_urls,
+    class_name: 'ShortenedUrl',
+    primary_key: :id,
+    foreign_key: :submitter_id
 end
