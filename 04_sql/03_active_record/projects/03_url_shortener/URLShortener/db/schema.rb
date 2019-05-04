d3eede6bd579e479ae080a7e48e220f0c924c24d
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-05-04 23:09:02 +0200

    Add Visits migration

--- 04_sql/03_active_record/projects/03_url_shortener/URLShortener/db/schema.rb
+++ 04_sql/03_active_record/projects/03_url_shortener/URLShortener/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_150640) do
+ActiveRecord::Schema.define(version: 2019_05_04_203616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2019_05_02_150640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer "shortened_url_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shortened_url_id"], name: "index_visits_on_shortened_url_id"
+    t.index ["user_id"], name: "index_visits_on_user_id"
   end
 
 end
